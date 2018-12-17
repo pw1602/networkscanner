6eafc74de2f859fcdda20ca7b6f206cc89a5cce1
pw1602 <pw1602@gmail.com>
2018-12-17 16:00:52 +0100

    Dodanie komentarzy

--- public/js/index.js
+++ public/js/index.js
@@ -2,25 +2,25 @@
 
 const COLLAPSE_DIV_PREFIX = 'Collapse_';
 
-Io.on('getDevices', (devices) => {
+Io.on('getDevices', (devices) => { // Otrzymanie informacji od serwera z urządzeniami
     const tmp = $('#computers');
-    tmp.empty();
+    tmp.empty(); // Wyczyszczenie tabeli urządzeń na stronie
 
-    devices.forEach((device, index) => {
+    devices.forEach((device, index) => { // Dla każdego urządzenia
         device.index = index;
-        $('#computers').append(getListElement(device));
+        $('#computers').append(getListElement(device)); // Dodanie urządzenia do tabeli
     });
 });
 
-function getListElement(value) {
+function getListElement(value) { // Ustawienia elementu dodawanego do listy
     return `<li id="${value.index}" title="${value.hostname}" class="list-group-item" data-toggle="collapse" data-target="#${COLLAPSE_DIV_PREFIX + value.index}" aria-expanded="false" aria-controls="${COLLAPSE_DIV_PREFIX + value.index}">${value.hostname} ${getBadge(value)}</li>` + getCollapse(value);
 }
 
-function getBadge(value) {
+function getBadge(value) { // Ustawienia badge
     return `<span class="float-right badge badge-light">${value.ip}</span>`;
 }
 
-function getCollapse(value) {
+function getCollapse(value) { // Ustawienia elementu na stronie, który po kliknięciu w urządzenie na lisćie będzie się pokazywał
     return `<div id="${COLLAPSE_DIV_PREFIX + value.index}" title="${value.hostname}" class="collapse bg-light text-dark"><span id="logs"></span><br/><span id="live">${getCollapseText(value)}</span></div>`;
 }
 
--- start.js
+++ start.js
@@ -2,32 +2,34 @@ const Express = require('express');
 const App = Express();
 const Http = require('http').Server(App);
 const SocketIo = require('socket.io')(Http);
-const NetworkList = require('network-list');
+const NetworkList = require('network-list'); // Odpowiada za szukanie komputerów w sieci
 
 const VIEWS = {
-    home: __dirname + '/public/views/index.html',
+    home: __dirname + '/public/views/index.html', // Ustawienie ścieżki do głownej strony
 }
 
-App.enable('trust proxy');
+App.enable('trust proxy'); // https://expressjs.com/en/guide/behind-proxies.html
+
+// Ustawienie folderów, aby nie podawać całej ścieki do plików w .html
 App.use(Express.static(__dirname + '/public'));
 App.use(Express.static(__dirname + '/node_modules'));
 
-App.get('/', (req, res) => res.sendFile(VIEWS.home));
+App.get('/', (req, res) => res.sendFile(VIEWS.home)); // W momencie wejścia na stronę wysyła idex.html
 
-const IP = '192.168.1';
-const DEVICES = [];
+const IP = '192.168.1'; // Po jakim ip ma szukać komputerów (bez ostatniej wartości)
+const DEVICES = []; // Tablica przechowująca wszystkie znalezione sprzęty
 
-Http.listen(80, () => {
+Http.listen(80, () => { // Uruchomienie serwera http
     console.log("App listening on port 80!");
 
     console.log("Scanning local network...");
-    NetworkList.scanEach({ip: IP}, (err, obj) => {
-        if (obj.alive) {
-            DEVICES.push(obj);
+    NetworkList.scanEach({ip: IP}, (err, obj) => { // Zaczęcie szukanai urządzeń.
+        if (obj.alive) { // Jeśli urządzenie działa
+            DEVICES.push(obj); // Dodaje urządzenie do tablicy
         }
     });
 });
 
-SocketIo.on('connection', socket => {
-    socket.emit("getDevices", DEVICES);
+SocketIo.on('connection', socket => { // W momencie kiedy ktoś połączy się ze stroną
+    socket.emit("getDevices", DEVICES); // Wysyła wszystkie odnalezione urządzenia do podłączonego klienta
 });
